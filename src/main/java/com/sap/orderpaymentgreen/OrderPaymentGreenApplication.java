494f06be19c8486413601a069d352886c83eb243
avigail <avigail4192@gmail.com>
2023-06-19 13:19:41 +0300

    Initial commit

---
+++ src/main/java/com/sap/orderpaymentgreen/OrderPaymentGreenApplication.java
@@ -0,0 +1,13 @@
+package com.sap.orderpaymentgreen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderPaymentGreenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderPaymentGreenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sap/orderpaymentgreen/OrderPaymentGreenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sap.orderpaymentgreen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderPaymentGreenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
