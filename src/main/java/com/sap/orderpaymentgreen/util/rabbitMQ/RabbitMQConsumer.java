b4c5298d06833bf58b773fa12030c43b5bf00c4d
fraidy1 <fraidyge@gmail.com>
2023-08-16 20:03:36 +0300

    RabbitMQ

--- src/main/java/com/sap/orderpaymentgreen/util/rabbitMQ/RabbitMQConsumer.java
+++ src/main/java/com/sap/orderpaymentgreen/util/rabbitMQ/RabbitMQConsumer.java
@@ -1,5 +1,7 @@
 package com.sap.orderpaymentgreen.util.rabbitMQ;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sap.orderpaymentgreen.dto.OrderDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Service;
@@ -9,16 +11,18 @@ import static com.sap.orderpaymentgreen.util.rabbitMQ.MessagingLoggingUtil.logRe
 
 @Service
 public class RabbitMQConsumer {
-
+    ObjectMapper objectMapper = new ObjectMapper();
     @RabbitListener(queues = {QUEUE_WAITING_FOR_CHARG})
-    public void listenOnWaitingChargQueue(OrderDTO order) {
-        logReceivedMessage(QUEUE_WAITING_FOR_CHARG, order);
-        MessagingLoggingUtil.logReceivedMessage(QUEUE_WAITING_FOR_CHARG,order);
+    public void listenOnWaitingChargQueue(String order) throws JsonProcessingException {
+        OrderDTO orderDTO = objectMapper.readValue(order, OrderDTO.class);
+        logReceivedMessage(QUEUE_WAITING_FOR_CHARG, orderDTO);
+        MessagingLoggingUtil.logReceivedMessage(QUEUE_WAITING_FOR_CHARG,orderDTO);
     }
 //@RabbitListener(queues = {QUEUE_WAITING_FOR_CHARG})
 //public void listenOnWaitingChargQueue(String order) {
 //    System.out.println("💕💕💕💕💕");
-//   // logReceivedMessage(QUEUE_WAITING_FOR_CHARG, order);
+//    System.out.println(order);
+//    // logReceivedMessage(QUEUE_WAITING_FOR_CHARG, order);
 //   // MessagingLoggingUtil.logReceivedMessage(QUEUE_WAITING_FOR_CHARG,order);
 //}
 }
